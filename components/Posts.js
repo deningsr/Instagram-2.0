b356e33c52758d48d78c27a185035beb7a13a12a
Denin Grcic <deningrcic2@gmail.com>
2021-12-07 21:05:17 -0800

    Created Posts container component

---
+++ components/Posts.js
@@ -0,0 +1,37 @@
+import Post from "./Post";
+
+const posts = [
+  {
+    id: "123",
+    username: "dgrcic",
+    userImg: "https://links.papareact.com/3ke",
+    img: "https://links.papareact.com/3ke",
+    caption: "This is dope!",
+  },
+  {
+    id: "123",
+    username: "dgrcic",
+    userImg: "https://links.papareact.com/3ke",
+    img: "https://links.papareact.com/3ke",
+    caption: "This is dope!",
+  },
+];
+
+function Posts() {
+  return (
+    <div>
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          username={post.username}
+          userImg={post.userImg}
+          img={post.img}
+          caption={post.caption}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Posts;
