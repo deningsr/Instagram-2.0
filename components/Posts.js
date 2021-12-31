13170418876b6d3892fdad1d36130397d16706ee
Denin Grcic <deningrcic2@gmail.com>
2021-12-30 20:33:25 -0800

    Rendered users posts to feed

--- components/Posts.js
+++ components/Posts.js
@@ -2,49 +2,32 @@ import Post from "./Post";
 import { useState, useEffect } from "react";
 import { db } from "../firebase";
 import { onSnapshot, collection, query, orderBy } from "@firebase/firestore";
-import { onSnapshot } from "firebase/firestore/dist/firestore";
-
-const posts = [
-  {
-    id: "123",
-    username: "dgrcic",
-    userImg: "https://links.papareact.com/3ke",
-    img: "https://links.papareact.com/3ke",
-    caption: "This is dope!",
-  },
-  {
-    id: "123",
-    username: "dgrcic",
-    userImg: "https://links.papareact.com/3ke",
-    img: "https://links.papareact.com/3ke",
-    caption: "This is dope!",
-  },
-];
 
 function Posts() {
   const [posts, setPosts] = useState([]);
 
-  useEffect(() => {
-    const unsubscribe = onSnapshot(
-      query(collection(db, "posts"), orderBy("timestamp", "desc")),
-      (snapshot) => {
-        setPosts(snapshot.docs);
-      }
-    );
-    return () => {
-      unsubscribe();
-    };
-  }, []);
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    []
+  );
+
+  console.log(posts);
   return (
     <div>
       {posts.map((post) => (
         <Post
           key={post.id}
           id={post.id}
-          username={post.username}
-          userImg={post.userImg}
-          img={post.img}
-          caption={post.caption}
+          username={post.data().username}
+          userImg={post.data().profileImg}
+          img={post.data().image}
+          caption={post.data().caption}
         />
       ))}
     </div>
